a507d06caa19a4c66e4f4508c7d18202e3a29cf7
Tony-S121 <t.smith9300@outlook.com>
2024-05-02 12:20:46 +0100

    Fixed variable name

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
         ingredients_string+=ingredient + " "
 
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        st.write('The search value for ', ingredient, ' is ', search_on, '.')
 
         st.subheader(ingredient + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
