82ae50a933c20fa0f26ba1763d0c59f90c55fa20
Tony-S121 <t.smith9300@outlook.com>
2024-05-02 12:03:51 +0100

    Moved API call within ingredient_chosen block

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,16 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections = 5)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
 if ingredients_list:
     st.write(ingredients_list)
     st.text(ingredients_list)
+
     ingredients_string=''
+
     for ingredient in ingredients_list:
         ingredients_string+=ingredient + " "
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(name_on_order, ingredients)
