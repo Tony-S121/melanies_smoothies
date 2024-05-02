293e55ee78c63b6ad38019f7686ce27c8f8f3e13
Tony-S121 <t.smith9300@outlook.com>
2024-05-02 12:19:35 +0100

    Use search_on to populate dynamic API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
+##st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections = 5)
@@ -40,8 +40,12 @@ if ingredients_list:
 
     for ingredient in ingredients_list:
         ingredients_string+=ingredient + " "
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+
         st.subheader(ingredient + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + ingredient)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     st.write(ingredients_string)
 
