912266fbc5c450969b3ab1af32148e4f8480019b
Tony-S121 <t.smith9300@outlook.com>
2024-05-02 12:05:11 +0100

    Made API call dynamic and added subheader for nutrition info

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,8 @@ if ingredients_list:
 
     for ingredient in ingredients_list:
         ingredients_string+=ingredient + " "
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(ingredient + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + ingredient)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     st.write(ingredients_string)
 
